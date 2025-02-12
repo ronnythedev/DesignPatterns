afa3d40684c8762a38761c0791386feba63286b7
Ronny Delgado <delgado.ronny@gmail.com>
2025-02-12 11:25:22 -0600

    feat: first observer pattern example without refactor

---
+++ ObserverPattern01_BR/ObserverPattern01_BR/Business/Model/DragonEventManager.cs
@@ -0,0 +1,57 @@
+namespace ObserverPattern01_BR.Business.Model;
+
+public class DragonEventManager
+{
+    private readonly Knight _knight;
+    private readonly Mage _mage;
+    private readonly Merchant _merchant;
+    
+    public DragonEventManager(Knight knight, Mage mage, Merchant merchant)
+    {
+        _knight = knight;
+        _mage = mage;
+        _merchant = merchant;
+    }
+
+    public void DragonAttacks(int dragonPower)
+    {
+        // Here is the problem: DragonEventManger is responsible for all what happens when a dragon attacks.
+        // This breaks the Single Responsibility Principle.
+        // Imagine if the game has 100 different characters, each with different reactions to a dragon attack!
+        Console.WriteLine($"Dragon attacks with power {dragonPower}!");
+        _knight.React();
+        _mage.React(dragonPower);
+        _merchant.React();
+    }
+}
+
+public class Knight
+{
+    public void React()
+    {
+        Console.WriteLine("Knight draws his sword!");
+    }
+}
+
+public class Mage
+{
+    public void React(int dragonPower)
+    {
+        if (dragonPower > 50)
+        {
+            Console.WriteLine("Mage casts a fireball!");    
+        }
+        else
+        {
+            Console.WriteLine("Mage uses a simple magic bolt.");
+        }
+    }
+}
+
+public class Merchant
+{
+    public void React()
+    {
+        Console.WriteLine("Merchant hides behind a barrel!");
+    }
+}
---
+++ ObserverPattern01_BR/ObserverPattern01_BR/Program.cs
@@ -0,0 +1,9 @@
+﻿using ObserverPattern01_BR.Business.Model;
+
+var knight = new Knight();
+var mage = new Mage();
+var merchant = new Merchant();
+
+var app = new DragonEventManager(knight, mage, merchant);
+
+app.DragonAttacks(100);
