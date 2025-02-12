7cb75a0fb37c15ad7edf84782ee929f00d78a0a5
Ronny Delgado <delgado.ronny@gmail.com>
2025-02-12 11:08:06 -0600

    feat: explain the problem before refactor

--- StrategyPattern01_BR/Business/Models/NavigationApp.cs
+++ StrategyPattern01_BR/Business/Models/NavigationApp.cs
@@ -2,6 +2,8 @@ namespace StrategyPattern01_BR.Business.Models;
 
 public class NavigationApp
 {
+    // Here is the problem: When we need a new type of route, we need to add a new method to this class.
+    // This breaks the Open/Closed Principle.
     public List<string> CalculateDrivingRoute(string start, string end)
     {
         Console.WriteLine("Calculating driving route...");
