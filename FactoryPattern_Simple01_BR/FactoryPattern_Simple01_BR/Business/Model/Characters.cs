de9d7f8d0ee6239c19202f9af81dceecb4e82b57
Ronny Delgado <delgado.ronny@gmail.com>
2025-02-26 17:06:46 -0600

    feat: change action of character in the before refactored code

--- FactoryPattern_Simple01_BR/FactoryPattern_Simple01_BR/Business/Model/Characters.cs
+++ FactoryPattern_Simple01_BR/FactoryPattern_Simple01_BR/Business/Model/Characters.cs
@@ -2,15 +2,15 @@ namespace FactoryPattern_Simple01_BR.Business.Model;
 
 public class Wizard
 {
-    public string PerformAction() => "Wizard created! Casting a fireball...";
+    public string PerformAction() => "Wizard ready! Casting a fireball...";
 }
 
 public class Knight
 {
-    public string PerformAction() => "Knight created! Charging into battle...";
+    public string PerformAction() => "Knight ready! Charging into battle...";
 }
 
 public class Rogue
 {
-    public string PerformAction() => "Rogue created! Sneaking in the shadows...";
+    public string PerformAction() => "Rogue ready! Sneaking in the shadows...";
 }
